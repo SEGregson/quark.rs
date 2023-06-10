d0eb345ff0af82bdb9dc6fc4b5c5f46a9d076a2e
SEGregson <skyeegregson@gmail.com>
2023-06-10 17:30:04 +0100

    completed tests, delete after requires bug fixing

--- quark/src/lib.rs
+++ quark/src/lib.rs
@@ -7,10 +7,20 @@ mod tests {
     mod linked_lists_tests {
         use crate::structures::linked_list::LinkedList;
 
+
         #[test]
         fn declaration() {
             let list = LinkedList::new(3);
             assert_eq!(3, list.get_start());
+        }
+
+        #[test]
+        fn size() {
+            let mut list = LinkedList::new(3);
+            list.insert_last(4);
+            list.insert_last(3);
+            list.insert_last(7);
+            assert_eq!(4, list.get_size());
         }
 
         #[test]
@@ -21,13 +31,35 @@ mod tests {
             assert_eq!(" -> 3 -> 4", list.to_string());
         }
 
+        
+
         #[test]
-        fn size() {
+        fn insert_first() {
+            let mut list = LinkedList::new(3);
+            list.insert_first(4);
+            // println!("{}", list.to_string());
+            assert_eq!(" -> 4 -> 3", list.to_string());
+        }
+
+        // forms deadlock
+        #[test]
+        fn delete_first_value() {
             let mut list = LinkedList::new(3);
             list.insert_last(4);
             list.insert_last(3);
             list.insert_last(7);
-            assert_eq!(4, list.get_size());
+            list.delete_first_value(4);
+            assert_eq!(" -> 3 -> 3 -> 7", list.to_string());
+        }
+
+        #[test]
+        fn delete_after() {
+            let mut list = LinkedList::new(3);
+            list.insert_last(4);
+            list.insert_last(3);
+            list.insert_last(7);
+            list.delete_after(2);
+            assert_eq!(" -> 3 -> 4", list.to_string());
         }
     }
 
